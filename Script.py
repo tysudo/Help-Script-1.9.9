c644dd2633f2d54eba85d29e5bf2d248f2ec3383
tysudo <91703892+tysudo@users.noreply.github.com>
2021-11-19 20:08:06 -0500

    Fixed bug.
    if pick==7:
        var4 = str(input(f'{Fore.GREEN}Are you are Mac or Linux. Case sensitive : '))
        if var4=="Mac":
            os.system('sh ./install_mac.sh')
        if var4=="Linux":
            os.system('sudo sh ./install_linux.sh')
        else:
            print(f'{Fore.RED}FATAL ERROR!!!')

--- Script.py
+++ Script.py
@@ -16,7 +16,7 @@ print('(3)List directory of choice')
 print('(4)Open Google Chrome and search. Must be webpage. example youtube.com')
 print('(5)Open all my favorite apps')
 print('(6)Open cool webpage')
-print('(7)If you have not installed the install script press 9')
+print('(7)If you have not installed the install script press 7')
 print('(8)Enter Windows CMD with wine')
 print('(9)Not sus at all? ? ? ?')
 print('(10)clone some thing off Github')
@@ -24,7 +24,7 @@ print('(11)Watch funny video to make you happier')
 print('(12)Cool music to listen ')
 
 #
-#
+#pick
 pick = int(input(f"{Fore.GREEN + Style.BRIGHT}Pick what you want to do: "))
 if pick==1:
     os.system('sl')
@@ -41,27 +41,26 @@ if pick==5:
 if pick==6:
     os.system('google-chrome https://www.lingscars.com/')
 if pick==7:
-    var4 = int(input(f'{Fore.GREEN}Are you are Mac or Linux. Press 1 on if on Mac. Press 2 if on linux: '))
-    if var4==1:
+    var4 = str(input(f'{Fore.GREEN}Are you are Mac or Linux. Case sensitive : '))
+    if var4=="Mac":
         os.system('sh ./install_mac.sh')
-    if var4==2:
+    if var4=="Linux":
         os.system('sudo sh ./install_linux.sh')
     else:
         print(f'{Fore.RED}FATAL ERROR!!!')
 if pick==8:
     os.system('wine cmd')
-if pick==8:
-    os.system('say oga boga. oga boga ')
 if pick==9:
+    os.system('say oga boga. oga boga')
+if pick==10:
     var5 = input ('What repo do you wan to clone: ')
     os.system('git clone '+var5+' cloned')
     print(f'{Fore.RED}You cloned it in a folder called cloned')
-if pick==10:
+if pick==11:
     os.system('google-chrome https://www.youtube.com/watch?v=R-RZ1OrjtQw')
 if pick==12:
     os.system('google-chrome https://www.youtube.com/watch?v=Et3G7JSw4fQ')
-if pick==13:
-    os.system('google-chrome ')
+#pick end
 
     
 #  
