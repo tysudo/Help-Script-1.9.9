ab6ae350d50adb362a2deb9fae834d5594ac538f
tysudo <91703892+tysudo@users.noreply.github.com>
2021-11-21 11:32:41 -0500

    Renamed Script-2.0.py

--- Script-BETA.py
+++ Script-BETA.py
@@ -1,5 +1,3 @@
-#did this comment work?
-
 import os
 import sys
 import tkinter
@@ -20,13 +18,14 @@ print('Install')
 print('Other')
 print('System')
 pick = str(input(f'{Fore.GREEN}What do you want to do: '))
+#eeeeeeeeeeeeeeeeeeeeeee
 if pick == "Search":
     print(f'{Fore.GREEN}(1)Search Amazon.com')
     print(f'{Fore.GREEN}(2)Search DuckDuckGo')
     print(f'{Fore.GREEN}(3)Search YouTube')
     print(f'{Fore.GREEN}(4)Search NewEgg')
     print(f'{Fore.GREEN}(5)Search Google')
-    search = int(input('What do you want to do: '))
+    search = int(input(f'{Fore.GREEN}What do you want to do: '))
     if search == 1:
         var1 = str(input('What would you like to search for. Spaces must be +: '))
         os.system('google-chrome https://www.amazon.ca/s?k='+var1+'')
@@ -42,11 +41,13 @@ if pick == "Search":
     if search == 5:
         var5 = str(input('What would you like to search for. Spaces must be +: '))
         os.system('google-chrome https://www.google.com/search?q='+var5+'')
+#eeeeeeeeeeeeeeeeeee
 if pick == "Wine":
-    print('(1)Wine CMD')
-    wine = int(input('What would you like to do: '))
+    print(f'{Fore.GREEN}(1)Wine CMD')
+    wine = int(input(f'{Fore.GREEN}What would you like to do: '))
     if wine == 1:
         os.system('wine cmd')
+#eeeeeeeeeeeeeeeeeeeeeeeeee
 if pick == "Install":
     print('(1)Install script')
     var6 = str(input(f'{Fore.GREEN}Are you are Mac or Linux. cAsE SeNsItIvE : '))
@@ -54,12 +55,13 @@ if pick == "Install":
         os.system('sh ./install_mac.sh')
     if var6=="Linux":
         os.system('sudo sh ./install_linux.sh')
+#eeeeeeeeeeeeeeeeeeeeeeeee
 if pick == "Other":
-    print('(1)Not sus at all')
-    print('(2)coolish webpage')
-    print('(3)Cool music to listen to')
-    print('(4)Funny video to make you happier')
-    Other = int(input('what would you like to do: '))
+    print(f'{Fore.GREEN}(1)Not sus at all')
+    print(f'{Fore.GREEN}(2)coolish webpage')
+    print(f'{Fore.GREEN}(3)Cool music to listen to')
+    print(f'{Fore.GREEN}(4)Funny video to make you happier')
+    Other = int(input(f'{Fore.GREEN}What would you like to do: '))
     if Other == 1:
         os.system('say oga boga. oga boga')
     if Other == 2:
@@ -67,14 +69,26 @@ if pick == "Other":
     if Other == 3:
         os.system('google-chrome https://www.youtube.com/watch?v=Et3G7JSw4fQ')
     if Other == 4:
-        os.system('')
-    
+        os.system('google-chrome https://www.youtube.com/watch?v=R-RZ1OrjtQw')
+#eeeeeeeeeeeeeeeeeeeeeeeeeee
 if pick == "System":
+    print(f'{Fore.GREEN}(1)Run train')
+    print(f'{Fore.GREEN}(2)HTOP')
+    print(f'{Fore.GREEN}(3)Search directory')
+    print(f'{Fore.GREEN}(4)Clone somthing off of Github')
     System = int(input('what would you like to do: '))
-    print('(1)Run train')
-    print('(2)HTOP')
-    print('(3)Search directory')
-    print('(4)Clone somthing off of Github')
+    if System == 1:
+        os.system('sl')
+    if System == 2:
+        os.system('htop')
+    if System == 3:
+        var7 = str(input(f'{Fore.GREEN}What directory would you like to search: '))
+        os.system('ls '+var7+'')
+    if System == 4:
+        var8 = str(input(f'{Fore.GREEN}What would you like to clone: '))
+        os.system('git clone '+var8+' cloned')
+        
+        
     
         
     
