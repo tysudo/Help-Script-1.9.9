2a6722ea960ea93679afd201e9d53d243ecc02b9
tysudo <91703892+tysudo@users.noreply.github.com>
2021-11-21 10:32:18 -0500

    Create Script-2.0.py

---
+++ Script-2.0.py
@@ -0,0 +1,56 @@
+import os
+import sys
+import tkinter
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+import argparse
+import scrapy
+#args
+parser = argparse.ArgumentParser(description='This is a script that will make your day faster.')
+args = parser.parse_args()
+
+
+print('Search')
+print('Wine')
+print('Install')
+print('Other')
+print('System commands')
+pick = str(input(f'{Fore.GREEN}What do you want to do: '))
+if pick == "Search":
+    print(f'{Fore.GREEN}(1)Search Amazon.com')
+    print(f'{Fore.GREEN}(2)Search DuckDuckGo')
+    print(f'{Fore.GREEN}(3)Search YouTube')
+    print(f'{Fore.GREEN}(4)Search NewEgg')
+    print(f'{Fore.GREEN}(5)Search Google')
+    search = int(input('What do you want to do: '))
+    if search == 1:
+        var1 = str(input('What would you like to search for. Spaces must be +: '))
+        os.system('google-chrome https://www.amazon.ca/s?k='+var1+'')
+    if search == 2:
+        var2 = str(input('What would you like to search for. Spaces must be +: '))
+        os.system('google-chrome https://duckduckgo.com/?q='+var2+' ')
+    if search == 3:
+        var3 = str(input('What would you like to search for. Spaces must be +: '))
+        os.system('google-chrome https://www.youtube.com/results?search_query='+var3+'')
+    if search == 4:
+        var4 = str(input('What would you like to search for. Spaces must be +: '))
+        os.system('google-chrome https://www.newegg.ca/p/pl?d='+var4+'')
+    if search == 5:
+        var5 = str(input('What would you like to search for. Spaces must be +: '))
+        os.system('google-chrome https://www.google.com/search?q='+var5+'')
+if pick == "Wine":
+    print('(1)Wine CMD')
+    wine = int(input('What would you like to do: '))
+    if wine == 1:
+        os.system('wine cmd')
+if pick == "Install":
+    print('(1)Install script')
+     var6 = str(input(f'{Fore.GREEN}Are you are Mac or Linux. cAsE SeNsItIvE : '))
+    if var6=="Mac":
+        os.system('sh ./install_mac.sh')
+    if var6=="Linux":
+        os.system('sudo sh ./install_linux.sh')
+if pick=="Other":
+    print('')
+        
